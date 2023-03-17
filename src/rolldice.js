f448483a6a9a1955f2ea7bf8dd92f497aae6b91f
Damlackr <damlacakr61@gmail.com>
2023-03-17 09:53:09 +0300

    first

---
+++ src/rolldice.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import dice1 from './images/Dice1.png';
+import dice2 from './images/Dice2.png';
+import dice3 from './images/Dice3.png';
+import dice4 from './images/Dice4.png';
+import dice5 from './images/Dice5.png';
+import dice6 from './images/Dice6.png';
+
+var list = [dice1, dice2, dice3, dice4, dice5, dice6];
+
+function RollingDice(){
+    const [rolledDice1, setRolledDice1] = useState(0);
+    const [rolledDice2, setRolledDice2] = useState(0);
+
+
+    const roll = () =>{
+        const newRandomNumber1 = Math.floor(Math.random() * 6) + 1;
+        const newRandomNumber2 = Math.floor(Math.random() * 6) + 1;
+        setRolledDice1(newRandomNumber1);
+        setRolledDice2(newRandomNumber2);
+
+    }
+    return(
+        <div>
+            <img src={list[rolledDice1-1]} width="100" height="100"/> 	&nbsp;
+            <img src={list[rolledDice2-1]} width="100" height="100"/> 	&nbsp;
+            <button onClick={roll} style={{backgroundColor:'pink'}}> Sallayınız</button> 
+            {rolledDice1 === rolledDice2 && <h2> Her iki zar birbirine eşit olduğu için kazandınız!</h2>}
+            
+        </div>
+    )
+}
+
+export default RollingDice;
